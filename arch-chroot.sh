701d73a41b196c010c2ed090655e5099abbca683
Anmol Jain <74057679+Anmol1184@users.noreply.github.com>
2022-02-07 10:20:26 +0530

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -12,23 +12,23 @@ echo root:password | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector linux-zen-headers avahi xdg-user-dirs xgd-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pulseaudio bash-completion openssh rsync ntfs-3g xorg sddm openbox lofi polybar nautilus gnome-terminal firefox xf86-video-amdgpu 
-pacman -S dmenu lxappearance nitrogen picom menumaker grub-btrfs
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools linux-zen-headers avahi xdg-user-dirs xgd-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pulseaudio bash-completion openssh rsync ntfs-3g xorg sddm openbox lofi polybar nautilus gnome-terminal firefox xf86-video-amdgpu 
+
 
 #AUR pkgs
 #timeshift timeshift-autosnap zram 
-
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin
+makepkg -si
 #pacman -S  --noconfirm xf86-video-amdgpu
 #pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-
+paru -Sy timeshift timeshift-autosnap zram
 systemctl enable NetworkManager
 #systemctl enable sshd
-systemctl enable avahi-daemon
 # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
 systemctl enable fstrim.timer
 #systemctl enable libvirtd
 #systemctl enable firewalld
@@ -41,7 +41,7 @@ echo aj:password | chpasswd
 usermod -aG wheel aj
 
 echo "aj ALL=(ALL) ALL" >> /etc/sudoers.d/aj
-
+pacman -S dmenu lxappearance nitrogen picom menumaker grub-btrfs
 
 printf "\e[1;32mDone! Add btrfs in MODULES=(), (nano /etc/mkinitcpio.conf) after this ( mkinitcpio -p linux ) Type exit, umount -a and reboot.\e[0m"
 
